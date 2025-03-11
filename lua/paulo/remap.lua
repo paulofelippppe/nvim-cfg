853fb594a94ac1ca48f815da362f98254b8cc6d0
Paulo Felippe <paulo.felippe222@gmail.com>
2025-03-10 21:58:18 -0300

    Remaps

--- lua/paulo/remap.lua
+++ lua/paulo/remap.lua
@@ -1,6 +1,9 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>tc", ":tabclose<CR>")
 
+-- Move the selected text upward or downward
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Exit terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
