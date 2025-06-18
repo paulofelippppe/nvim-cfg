3111cec92ee88527d5ef9339388a108b5210e732
paulofelippppe <paulo.felippe222@gmail.com>
2025-06-18 18:31:09 -0300

    nvim-tree now doesn't hide  files

--- lua/paulo/plugins/nvim-tree.lua
+++ lua/paulo/plugins/nvim-tree.lua
@@ -6,6 +6,11 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+            git = {
+                enable = true,
+                ignore = false,
+            }
+        })
 	end,
 }
